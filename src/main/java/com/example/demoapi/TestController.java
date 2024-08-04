e7aad5b621029be560e9c9d91b28cb81766188f5
Marcos Stival <markcosta15@hotmail.com>
2024-08-05 00:54:43 +0200

    Update Gh action

--- src/main/java/com/example/demoapi/TestController.java
+++ src/main/java/com/example/demoapi/TestController.java
@@ -1,7 +1,6 @@
 package com.example.demoapi;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,7 +8,7 @@ public class TestController {
 
     @GetMapping("/test")
     public String test() {
-        return "Hello, World!";
+        return "Hello, World! This is a test.";
     }
 
 }
