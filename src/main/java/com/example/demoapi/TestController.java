8d44af75325dafd6d8246def89be973d82efb8ed
Marcos Stival <markcosta15@hotmail.com>
2024-08-05 21:48:29 +0200

    Update test endpoint

--- src/main/java/com/example/demoapi/TestController.java
+++ src/main/java/com/example/demoapi/TestController.java
@@ -8,7 +8,7 @@ public class TestController {
 
     @GetMapping("/test")
     public String test() {
-        return "Hello, World! This is a test.";
+        return "Hello, World! This is a test. This is another test.";
     }
 
 }
