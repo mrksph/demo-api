1c8a916707bbf2096276483af2eff554afe89592
Marcos Stival <markcosta15@hotmail.com>
2024-08-28 23:06:55 +0200

    Update

--- src/main/java/com/example/demoapi/TestController.java
+++ src/main/java/com/example/demoapi/TestController.java
@@ -8,6 +8,7 @@ public class TestController {
 
     @GetMapping("/test")
     public String test() {
+        System.out.println("Test endpoint hit");
         return "Hello, World! This is a test. This is another test. One more test. Two tests";
     }
 
