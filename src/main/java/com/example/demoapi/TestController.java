c1d52ae621d590c3aa1bd703e42b449cecd40df2
Marcos Stival <markcosta15@hotmail.com>
2024-08-29 00:04:46 +0200

    Last try

--- src/main/java/com/example/demoapi/TestController.java
+++ src/main/java/com/example/demoapi/TestController.java
@@ -8,7 +8,7 @@ public class TestController {
 
     @GetMapping("/test")
     public String test() {
-        System.out.println("Test endpoint hit");
+        System.out.println("Test endpoint hit!");
         return "Hello, World! This is a test. This is another test. One more test. Two tests. Three. Four";
     }
 
